e878a37bfb7e582ecb8df25ab146ec50c18f8638
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-27 18:39:58 +0530

    helloworld

---
+++ CSPP2-practice/m1/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ *@author Madhavi.
+ */
+public final class HelloWorld {
+    /**
+     *Empty constructor.
+     */
+    private HelloWorld() {
+        //I am not using this constructor
+    }
+    /**
+     * main function to print the hello world.
+     * @param args is the parameter for this statement
+     */
+    public static void main(final String[] args) {
+        System.out.println("hello world");
+    }
+}
