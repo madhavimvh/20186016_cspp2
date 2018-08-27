1f9f06c34eed3143487287851d1263402b0ace39
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-27 19:22:25 +0530

    quotient_remainder

---
+++ CSPP2-practice/m1/Quotient_Remainder.java
@@ -0,0 +1,9 @@
+public final class Quotient_Remainder{
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		double quo = a/b;
+		int rem = a % b;
+		System.out.println(quo + " " + rem);
+	}
+}
