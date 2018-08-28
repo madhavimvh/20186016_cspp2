fa448121a61bb65c77d0ffa4eb18b91d415f2f95
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-28 17:08:28 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- CSPP2-assignments/m2/Assignment-2/Assignment-2/Solution.java
+++ CSPP2-assignments/m2/Assignment-2/Assignment-2/Solution.java
@@ -19,7 +19,7 @@ public class Solution {
 			double root2 = (-b - Math.sqrt(determinant))/(2*a);
 			System.out.println(root1 + " " + root2);
 		}
-		else if (determinant == 0){
+		else if(determinant == 0) {
 			double root = -b/2*a;
 			System.out.println(root + " " + root);
 		}
