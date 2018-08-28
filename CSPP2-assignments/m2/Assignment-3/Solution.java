56336a62b0b8321937f9eaf3638828903ae2008b
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-28 18:29:08 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignments/m2/Assignment-3/Solution.java
+++ CSPP2-assignments/m2/Assignment-3/Solution.java
@@ -1,33 +1,37 @@
 import java.util.Scanner;
 /**
-*importing scanner 
+*importing scanner.
 */
 public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);      
+    /**
+    *empty constructor
+    */
+    private Solution(){
+        
+    }
+    /*
+    Do not modify this main function.
+    */
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
         //double result=power(base,exponent);
         long result = power(base,exponent);
         System.out.println((long)result);
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
-	//public static double power(int base, int exponent) {
-	public static long power(long base, int exponent) {	
-		if (exponent == 0 ) {
-			return(1);
-		} else {
-			//double result = base * power(base, exponent - 1);
-			base = base * power(base, exponent - 1);
-			//return result;
-			return base;
-		}
-		
-		
-	}
+    }
+    /*
+    Need to write the power function and print the output.
+    */
+    //public static double power(int base, int exponent) {
+    public static long power(long base, int exponent) { 
+        if (exponent == 0 ) {
+            return(1);
+        } else {
+            //double result = base * power(base, exponent - 1);
+            base = base * power(base, exponent - 1);
+            //return result;
+            return base;
+        }
+    }
 }
