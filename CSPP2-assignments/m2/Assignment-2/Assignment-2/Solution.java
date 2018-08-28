a082c1e9f89eab4efc390a2da379fd13d9db64c0
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-28 17:18:05 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- CSPP2-assignments/m2/Assignment-2/Assignment-2/Solution.java
+++ CSPP2-assignments/m2/Assignment-2/Assignment-2/Solution.java
@@ -13,14 +13,14 @@ public class Solution {
 		//System.out.println(a + " " + b);
 	}
 	public static void rootsOfQuadraticEquation(int a, int b, int c) {
-		double determinant = (b*b) - (4*a*c);
+		int determinant = (b*b) - (4*a*c);
 		if (determinant > 0){
 			double root1 = (-b + Math.sqrt(determinant))/(2*a);
 			double root2 = (-b - Math.sqrt(determinant))/(2*a);
 			System.out.println(root1 + " " + root2);
 		}
 		else if(determinant == 0) {
-			double root = (-b/(2*a));
+			double root = (-b + Math.sqrt(determinant)/(2*a));
 			System.out.println(root + " " + root);
 		}
 	}
