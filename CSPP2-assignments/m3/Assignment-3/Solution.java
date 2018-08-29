ab28732b09ba9f64c30b670e5430c1c22a2ae5cc
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-29 15:16:26 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignments/m3/Assignment-3/Solution.java
+++ CSPP2-assignments/m3/Assignment-3/Solution.java
@@ -1,8 +1,8 @@
 
 import java.util.Scanner;
-/*
-    Do not modify this main function.
-    */
+/**
+ Do not modify this main function.
+*/
 public final class Solution {
     /**
      * empty constructor.
@@ -10,18 +10,18 @@ public final class Solution {
     private Solution() {
     }
     /**
-     * { function_description }
+     * { function_description }.
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner s=new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         //gcd(n1,n2);
         System.out.println(gcd(n1, n2));
     }
-    static int gcd(int n1, int n2) {
+    static int gcd(final int n1, final int n2) {
         int temp = 1;
         int i;
         for (i = 1;i <= n1 && i <= n2; i++) {
@@ -32,3 +32,4 @@ public final class Solution {
         return temp;
     }
 }
+
