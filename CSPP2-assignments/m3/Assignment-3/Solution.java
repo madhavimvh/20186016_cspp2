f06c6b68f12dd52030a0c54228dbc296b90a832e
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-29 15:17:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignments/m3/Assignment-3/Solution.java
+++ CSPP2-assignments/m3/Assignment-3/Solution.java
@@ -15,16 +15,24 @@ public final class Solution {
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         //gcd(n1,n2);
         System.out.println(gcd(n1, n2));
     }
+    /**
+     * { function_description }.
+     *
+     * @param      n1    The n 1
+     * @param      n2    The n 2
+     *
+     * @return     { description_of_the_return_value }
+     */
     static int gcd(final int n1, final int n2) {
         int temp = 1;
         int i;
-        for (i = 1;i <= n1 && i <= n2; i++) {
+        for (i = 1; i <= n1 && i <= n2; i++) {
             if (n1 % i == 0 && n2 % i == 0) {
                 temp = i;
             }
