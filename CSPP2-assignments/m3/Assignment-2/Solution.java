547797d2646dfa772cb984fbf1c6e15a19fe732c
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-29 14:27:07 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignments/m3/Assignment-2/Solution.java
+++ CSPP2-assignments/m3/Assignment-2/Solution.java
@@ -3,52 +3,54 @@
  */
 import java.util.Scanner;
 /*
- Do not modify this main function.
+*Do not modify this main function.
 */
 public class Solution {
-
     /**
-     * { function_description }
+    *Empty constructor.
+    */
+    private Solution() {
+    }
+    /**
+     * { function_description }.
      *
      * @param      args  The arguments
      */
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         System.out.println(returnSevenCount(n));
     }
-
     /**
      * Returns seven count.
      *
-     * @param      n     { parameter_description }
+     * @param      n     { parameter_description }.
      *
-     * @return     { description_of_the_return_value }
+     * @return     { description_of_the_return_value }.
      */
     public static int returnSevenCount(int n) {
-    	int i = 1; 
+        int i = 1; 
         int count = 0;
         for(i = 1; i <= n; i++) {
-        	count += returnCountOfNumber(i);
-	    }
-	    return count;
+            count += returnCountOfNumber(i);
+        }
+        return count;
     }
-
     /**
      * Returns a count of number.
      *
-     * @param      n     { parameter_description }
+     * @param      n     { parameter_description }.
      *
      * @return     { description_of_the_return_value }
      */
     public static int returnCountOfNumber(int n) {
-    	int count = 0;
-    	while(n > 0) {
-    		if (n % 10 == 7) {
-    			count += 1;
-    		}
-    		n = n/10;
-    	}
-    	return count;
+        int count = 0;
+        while(n > 0) {
+            if (n % 10 == 7) {
+                count += 1;
+            }
+            n = n/10;
+        }
+        return count;
     }
 }
