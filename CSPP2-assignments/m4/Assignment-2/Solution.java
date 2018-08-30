e093c38401d3a775338c08423520c2c9ceb7fc95
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-30 20:23:25 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- CSPP2-assignments/m4/Assignment-2/Solution.java
+++ CSPP2-assignments/m4/Assignment-2/Solution.java
@@ -15,27 +15,24 @@ public final class Solution {
      */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] arr1 = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        int n1 = sc.nextInt();
+        int m1 = sc.nextInt();
+        int[][] arr1 = new int[n1][m1];
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < m1; j++) {
                 arr1[i][j] = sc.nextInt();
             }
         }
-        int[][] arr2 = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        int n2 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int[][] arr2 = new int[n2][m2];
+        for (int i = 0; i < n2; i++) {
+            for (int j = 0; j < m2; j++) {
                 arr2[i][j] = sc.nextInt();
             }
         }
-        int[][] d = addition(arr1, arr2, n, m);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.println(d[i][j] + " ");
-            }
-            System.out.println(" ");
-        }
+        addition(arr1, arr2, n1, m1, n2, m2);
+
     }
     
     /**
@@ -48,15 +45,33 @@ public final class Solution {
      *
      * @return     { description_of_the_return_value }
      */
-    public static int[][] addition (final int[][] arr1, final int[][] arr2, final int n, final int m) {
-         int[][] c = new int[n][m];
-         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                c[i][j] = arr1[i][j] + arr2[i][j];
+    public static void addition (final int[][] arr1, final int[][] arr2, final int n1, final int m1, final int n2, final int m2) {
+        if (n1 == n1 && m1 ==m2) {
+            int[][] c = new int[n1][m1];
+                for (int i = 0; i < n1; i++) {
+                    for (int j = 0; j < m1; j++) {
+                        c[i][j] = arr1[i][j] + arr2[i][j];
+                }
             }
+            printMatrix(c, n1, m1);
+        } else {
+            System.out.println("not possible");
         }
-    return c;
-}
+    }
 
-
+    /**
+     * { function_description }.
+     *
+     * @param      d     { parameter_description }
+     * @param      n1    The n 1
+     * @param      m1    The m 1
+     */
+    public static void printMatrix(int[][] d, int n1, int m1) {
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < m1; j++) {
+                System.out.println(d[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
 }
