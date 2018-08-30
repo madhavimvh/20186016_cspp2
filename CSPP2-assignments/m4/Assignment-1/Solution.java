15d28fab71eba5f5cc4ee67d5c9938053abe65c1
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-30 14:38:57 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP2-assignments/m4/Assignment-1/Solution.java
+++ CSPP2-assignments/m4/Assignment-1/Solution.java
@@ -7,36 +7,43 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
+    /**
      * Empty constructor.
      */
     private Solution() {
     }
-	/**
-	*Fill this main function to print maximum of given array
-	*/
-	public static void main(final String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		//System.out.println("enter the size of array");
-		int n= sc.nextInt();
-		int[] arr = new int[n];
-		//System.out.println("enter the array elements");
-		for (int i = 0; i < n; i++) {
+    /**
+     * Fill this main function to print maximum of given array.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        //System.out.println("enter the size of array");
+        int n= sc.nextInt();
+        int[] arr = new int[n];
+        //System.out.println("enter the array elements");
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         //System.out.println(Arrays.toString(arr));
         System.out.println(maxnum(arr));
-		
-
-	}
-	public static int maxnum(final int[] arr) {
-		int max = arr[0];
-		for (int element : arr){
-			if (element > max) {
-				max = element;
-			}
-		}
-		return max;
-	}
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      arr   The arr
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int maxnum(final int[] arr) {
+        int max = arr[0];
+        for (int element : arr){
+            if (element > max) {
+                max = element;
+            }
+        }
+        return max;
+    }
 }
