e99b66c638b167c365549fbb943cc9d9c33c3b9d
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-30 14:31:43 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP2-assignments/m4/Assignment-1/Solution.java
+++ CSPP2-assignments/m4/Assignment-1/Solution.java
@@ -12,10 +12,10 @@ public final class Solution {
 	public static void main(final String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the size of array");
+		//System.out.println("enter the size of array");
 		int n= sc.nextInt();
 		int[] arr = new int[n];
-		System.out.println("enter the array elements");
+		//System.out.println("enter the array elements");
 		for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
