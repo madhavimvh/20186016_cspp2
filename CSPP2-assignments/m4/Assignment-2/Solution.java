3cc0c3fee5b960c240ad6cee0948998d58621b70
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-30 19:30:24 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- CSPP2-assignments/m4/Assignment-2/Solution.java
+++ CSPP2-assignments/m4/Assignment-2/Solution.java
@@ -3,67 +3,67 @@ import java.util.Scanner;
  * { item_description }.
  */
 public final class Solution {
-	/**
-	 * { item_description }.
-	 */
-	private Solution(){
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n1 = sc.nextInt();
-		int m1 = sc.nextInt();
-		int[][] arr1 = getMatrix(n1, m1);
-		System.out.println();
-		int n2 = sc.nextInt();
-		int m2 = sc.nextInt();
-		int[][] arr2 = getMatrix(n2, m2);
-		System.out.println(Addition(arr1, arr2, n1, m1));
-	}
-	/**
-	 * Gets the matrix.
-	 *
-	 * @param      n     { parameter_description }
-	 * @param      m     { parameter_description }
-	 *
-	 * @return     The matrix.
-	 */
-	public static int[][] getMatrix(final int n, final int m) {
-		Scanner scan = new Scanner(System.in);
-		int[][] arr = new int[n][m];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				arr[i][j] = scan.nextInt();
-			}
-		}
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				System.out.println(arr[i][j]);
-			}
-		}
-		return arr;
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      arr1  The arr 1
-	 * @param      arr2  The arr 2
-	 * @param      n     { parameter_description }
-	 * @param      m     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static int[][] Addition(final int[][] arr1, final int[][] arr2, final int n, final int m) {
-		 int c[][] = new int[n][m];
-		 for (int i = 0; i < n; i++) {
-		 	for (int j = 0; j < m; j++) {
-		 		c[i][j] = arr1[i][j] + arr2[i][j];		
-		 }
-	}
-	return c;
+    /**
+     * { item_description }.
+     */
+    private Solution() {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int m1 = sc.nextInt();
+        int[][] arr1 = getMatrix(n1, m1);
+        System.out.println();
+        int n2 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int[][] arr2 = getMatrix(n2, m2);
+        System.out.println(Addition(arr1, arr2, n1, m1));
+    }
+    /**
+     * Gets the matrix.
+     *
+     * @param      n     { parameter_description }
+     * @param      m     { parameter_description }
+     *
+     * @return     The matrix.
+     */
+    public static int[][] getMatrix(final int n, final int m) {
+        Scanner scan = new Scanner(System.in);
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scan.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.println(arr[i][j]);
+            }
+        }
+        return arr;
+    }
+    /**
+     * { function_description }
+     *
+     * @param      arr1  The arr 1
+     * @param      arr2  The arr 2
+     * @param      n     { parameter_description }
+     * @param      m     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int[][] Addition(final int[][] arr1, final int[][] arr2, final int n, final int m) {
+         int c[][] = new int[n][m];
+         for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                c[i][j] = arr1[i][j] + arr2[i][j];
+         }
+    }
+    return c;
 }
 }
