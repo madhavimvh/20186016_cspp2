19cbb63a2e77faa87d4d5e738d86681aa5fd8c08
madhavimvh <mvhmadhavi@msitprogram.net>
2018-08-31 17:13:10 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignments/m4/Assignment-4/Solution.java
+++ CSPP2-assignments/m4/Assignment-4/Solution.java
@@ -6,38 +6,37 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-	/**
-	 * { item_description }.
-	 */
-	private Solution() {
-	}
-	/**
-	 * Do not modify the main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-		
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      s     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static String reverseString(String s) {
-		String reverse = ""  ;
-		for (int i = 1; i <= s.length(); i++) {
-			reverse += s.charAt(s.length() - i);
-			// System.out.println(s.length() - i);
-			// System.out.println(reverse);
-		}
-	return reverse;
-	}
+public final class Solution {
+    /**
+     * { item_description }.
+     */
+    private Solution() {
+    }
+    /**
+     * Do not modify the main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String reverseString(final String s) {
+        String reverse = ""  ;
+        for(int i = 1; i <= s.length(); i++) {
+            reverse += s.charAt(s.length() - i);
+            // System.out.println(s.length() - i);
+            // System.out.println(reverse);
+        }
+    return reverse;
+    }
 }
