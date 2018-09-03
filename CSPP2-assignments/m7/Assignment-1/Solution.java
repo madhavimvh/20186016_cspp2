7c246055e200af6885fc0817ada48d06e123039c
madhavimvh <mvhmadhavi@msitprogram.net>
2018-09-03 19:39:16 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignments/m7/Assignment-1/Solution.java
+++ CSPP2-assignments/m7/Assignment-1/Solution.java
@@ -1,26 +1,65 @@
 import java.util.*;
 class InputValidator{
+	private static final int SIX = 6;
 	private String s;
 	private int inputLength;
 
-	public InputValidator(String s) {
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      s            { parameter_description }
+	 * @param      inputLength  The input length
+	 */
+	public InputValidator(final String s, final int inputLength) {
 		this.s = s;
-		this.inputLength = s.length();
+		this.inputLength = inputLength;
 	}
 	public boolean validateData() {
-		return inputLength >= 6;
+		return inputLength >= SIX;
 	}
 
 }
-public class Solution
-{
-	public static void main(String args[])
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * @param      args  { parameter_description }.
+	 */
+	public static void main(final String args[])
     {
     	Scanner s=new Scanner(System.in);
     	String input=s.next();
-    	InputValidator i=new InputValidator(input);    	
+    	InputValidator i=new InputValidator(input, input.length());    	
     	System.out.println(i.validateData());
 
     }
 
 }
+//other way to write the same program.
+// class InputValidator{
+// 	private String s;
+// 	private int inputLength;
+
+// 	public InputValidator(String s) {
+// 		this.s = s;
+// 		this.inputLength = s.length();
+// 	}
+// 	public boolean validateData() {
+// 		return inputLength >= 6;
+// 	}
+
+// }
+// public class Solution
+// {
+// 	public static void main(String args[])
+//     {
+//     	Scanner s=new Scanner(System.in);
+//     	String input=s.next();
+//     	InputValidator i=new InputValidator(input);    	
+//     	System.out.println(i.validateData());
+
+//     }
+
+// }
+
