b45e382ca3e93373fa5599ac0b04438431fb1433
madhavimvh <mvhmadhavi@msitprogram.net>
2018-09-04 17:16:44 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- CSPP2-assignments/m8/CodecampList/List.java
+++ CSPP2-assignments/m8/CodecampList/List.java
@@ -1,5 +1,8 @@
 import java.io.BufferedInputStream;
 import java.util.Scanner;
+/**
+ * List of .
+ */
 public class List {
     //Implement all the methods mentioned to build a ListADT
         /**
@@ -10,7 +13,6 @@ public class List {
          * { var_description }.
          */
         private int size;
-
     /*
      * The goal for the list is to store items.
      * How are we going to store the items in the list?
@@ -32,11 +34,9 @@ public class List {
      * This is a hard concept to understand. Discuss with your mentor.
      *
     */
-    
     // declare a private int[]
     // don't create the array yet using new
     // that's the job of the List constructor
-
     /*
      * What are the other class variables needed for creating a list?
      * How about keeping track of the size of the list?
@@ -56,11 +56,9 @@ public class List {
      * to be accessed by the methods that are outside of the List class.
      * 
      */
-
     // declare a private int size
     // again, don't initialize it here
     // variable initialization should be done in the constructor
-
     /**
      * The purpose of the constructor is to initialize the class variables with
      * some default values.
@@ -68,20 +66,16 @@ public class List {
     public List() {
         this.a = new int[10];
         this.size = 0;
-
         // what are the two variables to be initialized here?
         // think about the private variables described above.
         // What should be the default values?
         // In the case of the list, it should be empty but
         // it should be initialized with an array size like 10
-
         // Think about the initial value for size.
         // How many items do we have in the list when you create it?
         // An empty list has how many items?
         // That is the initial value to use for size.
-
     }
-    
     /**
      * The add method does what the name suggests. Add an int item to the list.
      * The assumption is to store the item at the end of the list What is the
@@ -98,7 +92,6 @@ public class List {
         a[size] = item;
         size++;
     }
-
     /**
      * The size method returns the value of the size. The purpose of the method
      * is to announce the size of the list to the objects outside the list
@@ -111,7 +104,6 @@ public class List {
         // replace the code below to implement the size method
         return size;
     }
-
     /**
      * The remove method does what the name suggests. Removes an int item,
      * specified by the index argument, from the list It also does an additional
@@ -140,7 +132,6 @@ public class List {
             System.out.println("Invalid Position Exception");
         }
     }
-
     /**
      * Get method has to return the items that is at the index position passed
      * as an argument to the method. If the item doesn't exist then return a -1
@@ -161,7 +152,6 @@ public class List {
         }
         return -1;
     }
-
     /**
      * What happens when you print an object using println? Java provides a
      * method named toString that is internally invoked when an object variable
@@ -186,7 +176,6 @@ public class List {
         s += a[size-1] + "]";
         return s;
     } 
-    
     /**
      * Contains return true if the list has the item passed as an argument to
      * the method So, iterate through the list and return true if the item
@@ -198,10 +187,8 @@ public class List {
      */
     public boolean contains(int item) {
         // Replace the code below
-        return (indexOf(item) >= 0);
-        
+        return (indexOf(item) >= 0);  
     }
-
     /**
      * Returns the index of the first occurrence of the specified element in
      * this list, or -1 if this list does not contain the element.
@@ -219,7 +206,6 @@ public class List {
         }
         return -1;
     }
-
 	/**
      * { function_description }.
      *
