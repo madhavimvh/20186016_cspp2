04b9f9eea24caca6c63b66962e223a8fedcbbdee
madhavimvh <mvhmadhavi@msitprogram.net>
2018-09-05 10:23:44 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP2-assignments/m9/assignment 1.1/Assignment-1/Fibonacci.java
+++ CSPP2-assignments/m9/assignment 1.1/Assignment-1/Fibonacci.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 /*
- * In the previous module you have created List ADT.
- * ADTs are developed as reusable libraries
- * i.e., anyone who needs the ADT can use it in their program
- * A program that uses the ADT is called a "client" of the ADT
- * The client typically instantiates an object of the ADT
- * and invokes the public methods on the ADT object.
+ * In the previous module you have created List ADT. ADTs are developed as
+ * reusable libraries i.e., anyone who needs the ADT can use it in their program
+ * A program that uses the ADT is called a "client" of the ADT The client
+ * typically instantiates an object of the ADT and invokes the public methods on
+ * the ADT object.
  *
- * This assignment is to practice the idea of client to the ADT.
- * Client class is Fibonacci and the client needs to use the List ADT
- * to store fibonacci numbers in the List ADT.
- * So, Fibonacci is going to instantiate an object of List ADT
- * generate fibonacci numbers and add them to the list
- * by invoking the add method provided in the List ADT.
- *
+ * This assignment is to practice the idea of client to the ADT. Client class is
+ * Fibonacci and the client needs to use the List ADT to store fibonacci numbers
+ * in the List ADT. So, Fibonacci is going to instantiate an object of List ADT
+ * generate fibonacci numbers and add them to the list by invoking the add
+ * method provided in the List ADT.
+ */
+/**
+ * Class for fibonacci.
  */
 public class Fibonacci {
     /**
@@ -28,7 +28,14 @@ public class Fibonacci {
      *
      * Look for the hint.txt if some of the testcases fail.
      */
-    public static List fib(int n) {
+    /**
+     * { function_description }.
+     *
+     * @param      n     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static List fib(final int n) {
         // todo - complete this method
         List list = new List(n);
         int x = 0, y = 1, z;
@@ -42,7 +49,7 @@ public class Fibonacci {
         }
         return list;
     }
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         System.out.println(fib(n));
