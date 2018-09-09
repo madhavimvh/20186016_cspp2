b9330328c559a9c39b604b261a5ab5ade5be3766
madhavimvh <mvhmadhavi@msitprogram.net>
2018-09-09 13:50:35 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- CSPP2-assignments/m13/Assignment-1/Solution.java
+++ CSPP2-assignments/m13/Assignment-1/Solution.java
@@ -44,19 +44,19 @@ class Set {
 		str = str + set[i] + "]";
 		return str;
 	}
-	// public void resize() {
-	// 	System.out.println(size);
-	// 	set = Arrays.copyOf(set, 2 * size);
-	// }
+	public void resize() {
+		System.out.println(size);
+		set = Arrays.copyOf(set, 2 * size);
+	}
 	public void add(final int item) {
-		// if (size == set.length) {
-		// 	resize();
-		// }
+		if (size == set.length) {
+			resize();
+		}
 		for (int i = 0; i <= size; i++) {
 			System.out.println(item);
 			if (item != set[i]) {
+				set[size++] = item;
 			}
-			set[size++] = item;
 			System.out.println(Arrays.toString(set));
 		}
 	}
