55948a9aceb722d87aaeb144dda9252ebf2f0b81
madhavimvh <mvhmadhavi@msitprogram.net>
2018-09-12 19:19:01 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- CSPP2-assignments/m16/bookyourshow/Solution.java
+++ CSPP2-assignments/m16/bookyourshow/Solution.java
@@ -121,8 +121,12 @@ class BookYourShow {
             for (int i = 0; i < patronSize; i++) {
                 if (patrons[i].getmobileNumber().equals(mobileNumber)) {
                     System.out.println(mobileNumber + " " + movieName + " " + datetime);
+                } else {
+                    System.out.println("Invalid");
                 }
             }
+        } else {
+            System.out.println("Invalid");
         }
         
     }
