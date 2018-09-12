a262d710fb1b5785026e05e396447cef981b8853
madhavimvh <mvhmadhavi@msitprogram.net>
2018-09-12 19:38:40 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- CSPP2-assignments/m16/bookyourshow/Solution.java
+++ CSPP2-assignments/m16/bookyourshow/Solution.java
@@ -93,7 +93,7 @@ class BookYourShow {
                 for (int j = 0; j < custseats.length; j++) {
                     if (seats[i].equals(custseats[j]) ) {
                         count += 1;
-                        new  String().replace(seats[i], "N/A");
+                        seats[i].replace(seats[i], "N/A");
 
                     }
                 }
