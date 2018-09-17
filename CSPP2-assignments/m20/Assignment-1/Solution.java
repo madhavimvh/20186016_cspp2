20cdd5d5c382487a6df77ea5d8dfa7bc961dd7d0
madhavimvh <mvhmadhavi@msitprogram.net>
2018-09-17 14:02:24 +0530

    "check_style_score -> 4 of 13 passed. style score: 0/1 "

--- CSPP2-assignments/m20/Assignment-1/Solution.java
+++ CSPP2-assignments/m20/Assignment-1/Solution.java
@@ -262,7 +262,7 @@ public final class Solution {
     			System.out.println("Error! Correct answer choice number is out of range for " + tokens[0]);
     			return;
     		}
-    		if (Integer.parseInt(tokens[2]) <= 0) {
+    		if (Integer.parseInt(tokens[3]) <= 0) {
     			System.out.println("Invalid max marks for " + tokens[0]);
     			return;
     		}
