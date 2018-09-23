67f9cea48f3829f0bc2bf790577dafbfc3ea0443
madhavimvh <mvhmadhavi@msitprogram.net>
2018-09-23 14:12:14 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- CSPP2-assignments/Plagiarism/Stringmatching/Solution.java
+++ CSPP2-assignments/Plagiarism/Stringmatching/Solution.java
@@ -1,71 +1,193 @@
-import java.util.Scanner;
+/**
+ * String matching.
+ * @author Swapnika Vakacharla
+ */
 import java.io.File;
-import java.util.Arrays;
-class Stringmatching {
-	private File file1;
-	private File file2;
-	private String[] p;
-	private String[] q;
-	private int l1;
-	private int l2;
-	public Stringmatching(File filee1, File filee2) {
-		this.file1 = filee1;
-		this.file2 = filee2;
-		p = null;
-		q = null;
-		l1 = 0;
-		l2 = 0;
-		try {
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //not used.
+    }
+    /**
+     * variable declaration.
+     */
+    private static final int THIRTEEN = 13;
+    /**
+     * variable declaration.
+     */
+    private static final int HUNDRED = 100;
+    /**
+     * variable declaration.
+     */
+    private static final double TWOHUNDRED = 200.0;
+    /**
+     * to calculate lcs.
+     *
+     * @param      doc1  The document 1
+     * @param      doc2  The document 2
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int lcs(final String doc1, final String doc2) {
+        int lcsmax = 0, lcs = 0, temp = 0;
+        for (int indexi = 0; indexi < doc1.length() - 1; indexi++) {
+            int indexj = 0;
+            while (indexj < doc2.length() - 1) {
+                temp = indexi;
+                lcs = 0;
+                if (doc1.charAt(temp) == (doc2.charAt(indexj))
+                        && doc1.charAt(temp) != ' ') {
+                    while (doc1.charAt(temp) == (doc2.charAt(indexj)) && indexj
+                            < doc2.length() - 1 && temp < doc1.length() - 1) {
+                        lcs++;
+                        indexj++;
+                        temp++;
+                    }
+                    if (lcs > lcsmax) {
+                        lcsmax = lcs;
+                    }
+                } else {
+                    indexj++;
+                }
+            }
+        }
+        return lcsmax + 1;
+    }
+    /**
+     * to print result in given format.
+     *
+     * @param      matchpercentmat  The matchpercentmat
+     * @param      filelist         The filelist
+     */
+    public static void printResult(final float[][] matchpercentmat,
+                                   final File[] filelist) {
+        String[] fileListAsString = new String[filelist.length];
+        for (int i = 0; i < filelist.length; i++) {
+            for (int j = 0; i < filelist[i].toString().length(); j++) {
+                if (filelist[i].toString().charAt(j) == '\\') {
+                    fileListAsString[i] = filelist[i].toString()
+                                          .substring(j + 1);
+                    break;
+                }
+            }
+        }
+        String res = "         ";
+        for (String eachFile : fileListAsString) {
+            // res += eachFile + "\t";
+            int numberOfSpaces = THIRTEEN - eachFile.length();
+            for (int spindex = 0; spindex < numberOfSpaces; spindex++) {
+                res += " ";
+            }
+            res += eachFile;
+        } res += " \n";
+        for (int i = 0; i < fileListAsString.length; i++) {
+            res += fileListAsString[i];
+            for (int j = 0; j < fileListAsString.length; j++) {
+                // res += "\t" + matchpercentmat[i][j] + "\t";
+                int numberOfSpaces = THIRTEEN - (matchpercentmat[i][j] + "")
+                                     .length();
+                for (int spindex = 0; spindex < numberOfSpaces; spindex++) {
+                    res += " ";
+                }
+                res += matchpercentmat[i][j] + "";
 
-		Scanner sc1 = new Scanner(file1);
-		Scanner sc2 = new Scanner(file2);
-		String s = "";
-		while(sc1.hasNext()) {
-			s += sc1.nextLine();
-		}
-		l1 = s.length();
-		p = s.toLowerCase().replaceAll("[^a-z0-9 ]", "").split(" ");
-		// System.out.println(Arrays.toString(p));
-		String x = "";
-		while(sc2.hasNext()) {
-			x += sc2.nextLine();
-		}
-		l2 = x.length();
-		q = x.toLowerCase().replaceAll("[^a-z0-9 ]", "").split(" ");
-		// System.out.println(Arrays.toString(q));
-		} catch (Exception ex) {
-			System.out.println("file not found");
-		}
-	}
-	public int perMatch() {
-		int count = 1;
-		for (int i = 0; i < p.length; i++) {
-			for (int j = 0; j < q.length; j++) {
-				if (p[i].equals(q[j])) {
-					count = count * p[i].length();
-				}					
-			}
-		}
-		return count;
-	}
-	public int calculate(int count) {
-		float res;
-		int res1 = l1 + l2;
-		res = ((float)count / (float)res1) * 100;
-		return (int) res;
+            } res += " \n";
+        }
+        System.out.print(res);
+        float maxpercetmatch = 0;
+        String file1 = "", file2 = "";
+        for (int i = 0; i < filelist.length; i++) {
+            for (int j = 0; j < filelist.length; j++) {
+                if (i < j && maxpercetmatch < matchpercentmat[i][j]) {
+                    file1 = fileListAsString[i];
+                    file2 = fileListAsString[j];
+                    maxpercetmatch = matchpercentmat[i][j];
+                }
+            }
+        }
+        System.out.println("Maximum similarity is between "
+                           + file1 + " and " + file2);
+    }
+    /**
+     * main function.
+     *
+     * @param      args       The arguments
+     *
+     * @throws     Exception  { exception_description }
+     */
+    public static void main(final String[] args) throws Exception {
+        try {
+            Scanner scan = new Scanner(System.in);
+            String foldername = scan.next();
+            File folder = new File(foldername);
+            File[] filelist = folder.listFiles();
+            String[] strlist = new String[filelist.length];
+            // System.out.println(Arrays.toString(filelist));
+            try {
+                int filecount = 0;
+                for (File file : filelist) {
+                    Scanner filescan = new Scanner(file);
+                    String str = "";
+                    while (filescan.hasNextLine()) {
+                        str += filescan.nextLine() + " ";
+                    }
+                    // System.out.println(str);
+                    strlist[filecount++] = str.trim();
+                }
+                // System.out.println(Arrays.toString(strlist));
+            } catch (FileNotFoundException e) {
+                System.out.println("file not found");
+            }
 
-	}
+            float[][] matchpercentmat = new float[filelist.length]
+            [filelist.length];
+            for (int i = 0; i < filelist.length; i++) {
+                for (int j = 0; j < filelist.length; j++) {
+                    if (i == j) {
+                        matchpercentmat[i][j] = HUNDRED;
+                    } else {
+    // int lcs = 0 , lcstemp = 0;
+    // // System.out.println(strlist[i] + "\n" + strlist[j]);
+    // for (String eachwordi : strlist[i].replace(".", " ").split(" ")) {
+    //  for (String eachwordj : strlist[j].replace(".", " ").split(" ")) {
+    //      if (eachwordi.equals(eachwordj) && eachwordi.length() > lcs) {
+    //          lcs = eachwordi.length();
+    //      }
+    //  }
+    // }
+    // matchpercentmat[i][j] = (lcs * 200)
+    // / (strlist[i].length() + strlist[j].length());
 
+                        int lcsmax = 0;
+                        if (!(strlist[i].equals("") || strlist[j]
+                                .equals(""))) {
+                            if (strlist[i].length() > strlist[j]
+                                    .length()) {
+                                lcsmax = lcs(strlist[i], strlist[j]);
+                            } else {
+                                lcsmax = lcs(strlist[j], strlist[i]);
+                            }
+                        }
+                        matchpercentmat[i][j] = Math.round((lcsmax * TWOHUNDRED)
+                            / (strlist[i].length() + strlist[j].length()));
+                    }
+                }
+            }
+            // System.out.println(Arrays.toString(matchpercentmat));
+            // for (int i = 0; i < matchpercentmat.length; i++) {
+            //  System.out.println(Arrays.toString(matchpercentmat[i]));
+            // }
+            printResult(matchpercentmat, filelist);
+        } catch (Exception e) {
+            System.out.println("Empty Directory");
+        }
+    }
 }
-public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String line = sc.nextLine();
-		File folder = new File(line);
-		File[] listoffiles = folder.listFiles();
-		for (int i = 0; i < listoffiles.length - 1; i++) {
-			Stringmatching match = new Stringmatching(listoffiles[i], listoffiles[i + 1]);
-			System.out.println(match.calculate(match.perMatch()));
-		}
-	}
-}
+
